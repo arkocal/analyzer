80a00e1bdc0750664cdcb73b2451765a484a3e5c
Boro Stamenic <stamenicboro@gmail.com>
2025-04-29 22:02:15 +0200

    update

--- scripts/restart/graphs.py
+++ scripts/restart/graphs.py
@@ -1,9 +1,9 @@
-from turtle import color
 import matplotlib.pyplot as plt
 import numpy as np
 import yaml
 import os
 
+# no-data-race graphs
 def average_datarace():
     x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/Autotune_svcomp_datarace_timings.txt', delimiter=',', unpack=True, dtype=None)
     x2,y2,z2, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/conflist_svcomp_datarace_timings.txt', delimiter=',', unpack=True, dtype=None)
@@ -52,8 +52,119 @@ def goblint_datarace():
     plt.savefig(os.path.dirname(__file__) + "/graphs/goblint_datarace.png")
 
 
-def main():
-    goblint_datarace()
+# no-overflow graphs
+def average_nooverflow():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/Autotune_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+    x2,y2,z2, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/conflist_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+    x3,y3,z3 = np.genfromtxt(os.path.dirname(__file__) + '/data/goblint_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+
+    ax = plt.subplot(111)
+    ax.set_title('Average runtime for SV-COMP no-overflow')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Used configuration')
+    ax.bar('Autotune', np.average(z1), color='b', align='center')
+    ax.bar('Conflist', np.average(z2), color='r', align='center')
+    ax.bar('Goblint', np.average(z3), color='g', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/average_nooverflow.png")
     
+
+def autotune_nooverflow():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/Autotune_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for autotune on SV-COMP no-overflow')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='b', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/autotune_nooverflow.png")
+
+
+def conflist_nooverflow():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/conflist_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for conflist on SV-COMP no-overflow')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='r', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/conflist_nooverflow.png")
+    
+
+def goblint_nooverflow():
+    x1,y1,z1 = np.genfromtxt(os.path.dirname(__file__) + '/data/goblint_svcomp_nooverflow_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for goblint on SV-COMP no-overflow')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='g', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/goblint_nooverflow.png")
+
+#  unreach-call graphs
+def average_unreachcall():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/Autotune_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+    x2,y2,z2, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/conflist_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+    x3,y3,z3 = np.genfromtxt(os.path.dirname(__file__) + '/data/goblint_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+
+    ax = plt.subplot(111)
+    ax.set_title('Average runtime for SV-COMP unreach-call')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Used configuration')
+    ax.bar('Autotune', np.average(z1), color='b', align='center')
+    ax.bar('Conflist', np.average(z2), color='r', align='center')
+    ax.bar('Goblint', np.average(z3), color='g', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/average_unreachcall.png")
+    
+
+def autotune_unreachcall():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/Autotune_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for autotune on SV-COMP unreach-call')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='b', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/autotune_unreachcall.png")
+
+
+def conflist_unreachcall():
+    x1,y1,z1, _, _ = np.genfromtxt(os.path.dirname(__file__) + '/data/conflist_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for conflist on SV-COMP unreach-call')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='r', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/conflist_unreachcall.png")
+    
+
+def goblint_unreachcall():
+    x1,y1,z1 = np.genfromtxt(os.path.dirname(__file__) + '/data/goblint_svcomp_unreachcall_timings.txt', delimiter=',', unpack=True, dtype=None)
+    ax = plt.subplot(111)
+    ax.set_title('Measured runtime for goblint on SV-COMP unreach-call')
+    ax.set_ylabel('Runtime in seconds')
+    ax.set_xlabel('Analyzed file')
+    ax.set_ylim([0,5])
+    ax.bar(x1, z1, color='g', align='center')
+    plt.savefig(os.path.dirname(__file__) + "/graphs/goblint_unreachcall.png")
+
+
+def main():
+    #autotune_datarace()
+    #conflist_datarace()
+    #goblint_datarace()
+    #average_datarace()
+
+    #autotune_nooverflow()
+    #conflist_nooverflow()
+    #goblint_nooverflow()
+    #average_nooverflow()
+
+    #autotune_unreachcall()
+    #conflist_unreachcall()
+    #goblint_unreachcall()
+    #average_unreachcall()
+    return
+
 if __name__ == "__main__":
     main()
--- scripts/restart/restart.py
+++ scripts/restart/restart.py
@@ -15,8 +15,12 @@ def parser():
     parser.add_argument('-a', '--autotune', action='store_true', help='Auto adjust first config after every restart for LIMIT runs or RUNTIME seconds.')
     parser.add_argument('--a_limit', type=int, metavar='LIMIT', default=2, help='limit how many iterations autotune is used for.')
     parser.add_argument('--spec', metavar='property.prp', help='Path or string for a specification for SV-COMP.')
+    parser.add_argument('--version', action='store_true', help='Print Goblint version information.')
+    parser.add_argument('--architecture', help='Architecture information to pass to Gobilnt.')
+    parser.add_argument('--witness.yaml.validate', help='Architecture information to pass to Gobilnt.')
+    parser.add_argument('--witness.yaml.unassume', help='Architecture information to pass to Gobilnt.')
     parser.add_argument('-c','--conf', nargs='+', help='configurations to be used.')
-    parser.add_argument('-f','--file', required=True, help='file to be analyzed.')
+    parser.add_argument('-f','--file', help='file to be analyzed.')
 
     args = parser.parse_args()
     assert args.timeout > 0, "Timeout must be a positive integer."
@@ -28,35 +32,40 @@ def handler(signum, frame):
     print("Restart time limit reached!")
     raise Exception("timeout")
 
-def loop(conf, file, spec, autotune, runtime, timeout, runcount, verbose, timing=False):
+def loop(args, timing=False):
     firstrun = True
     firstrunTime = 0.0
-    error_message = "Error" if spec == None else "SV-COMP result: unknown"
+    error_message = "Error" if args.spec == None else "SV-COMP result: unknown"
     restart = "true"
     last_output = "empty"
 
+    runcount = args.a_limit if args.autotune else len(args.conf)
+
     # total allowed runtime, print last results if runtime is reached
     signal.signal(signal.SIGALRM, handler)
-    signal.alarm(runtime)
+    signal.alarm(args.runtime)
 
     # start of restart loop
     try:
         for i in range(runcount):
             # set loop args
-            if autotune and i > 0:
+            if args.autotune and i > 0:
                 c = "scripts/restart/autotune.conf"
             else:
-                c = conf[i]
+                c = args.conf[i]
 
             if i >= runcount - 1:
                 restart = "false"
             if i > 0:
                 firstrun = False
 
-            gob_args = [analyzer_path, '--set', 'restart.enabled', restart, '--set', 'restart.timeout', str(timeout), '--conf', c, file]
-            if verbose: gob_args.append("-v")
-            if autotune and i > 0: gob_args.extend(["--set", "restart.autotune", "true"])
-            if spec != None: gob_args.extend(["--set", "ana.specification", spec])
+            gob_args = [analyzer_path, '--set', 'restart.enabled', restart, '--set', 'restart.timeout', str(args.timeout), '--conf', c, args.file]
+            if args.verbose: gob_args.append("-v")
+            if args.autotune and i > 0: gob_args.extend(["--set", "restart.autotune", "true"])
+            if args.spec != None: gob_args.extend(["--set", "ana.specification", args.spec])
+            if args.architecture != None: gob_args.extend(["--set", "exp.architecture", args.architecture])
+            if getattr(args, "witness.yaml.validate") != None: gob_args.extend(["--set", "witness.yaml.validate", getattr(args, "witness.yaml.validate")])
+            if getattr(args, "witness.yaml.unassume") != None: gob_args.extend(["--set", "witness.yaml.unassume", getattr(args, "witness.yaml.unassume")])
 
             # call goblint
             print("Current configuration: " + c)
@@ -84,9 +93,11 @@ def loop(conf, file, spec, autotune, runtime, timeout, runcount, verbose, timing
 def main():
     args = parser()
 
-    runcount = args.a_limit if args.autotune else len(args.conf)
+    if args.version:
+        result = subprocess.run([analyzer_path, "--version"])
+        return
 
-    loop(args.conf, args.file, args.spec, args.autotune, args.runtime, args.timeout, runcount, args.verbose)
+    loop(args)
 
 
 if __name__ == "__main__":
